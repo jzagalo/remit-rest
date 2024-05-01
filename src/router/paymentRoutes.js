d8d640c2301e482603447e3119dab1028e53184d
Paul Ezaga <paulezaga@gmail.com>
2024-05-01 15:52:30 +0200

    Modifications

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -6,18 +6,30 @@ const stripe = require('stripe')(
 
 // router endpoints
 router.post('/intents', async (req, res) => {
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2023-10-16'}
+  );
+  
   try {
     // create a PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount, // Integer, usd -> pennies, eur -> cents
-      gateway: 'klarna',
-      currency: 'usd',
+      customer: customer.id,
+      currency: 'eur',
       automatic_payment_methods: {
         enabled: true,
       },
     });
     // Return the secret
-    res.json({ paymentIntent: paymentIntent.client_secret });
+    res.json({ 
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+      publishableKey: 'sk_test_51OlqZyH3cCklqfTbwcIVJts95VXuoEUL8e5NT1ej8Pw97kWpIibqLlayIL7DCpCgHww3NDBNGgfWRLoeIpL3uNjh00J95sj1fI'
+    
+    });
   } catch (e) {
     res.status(400).json({
       error: e.message,
